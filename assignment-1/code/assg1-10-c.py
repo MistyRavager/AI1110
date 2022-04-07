d1428de41c67decb253ce85813c943c15893ac0e
Kushagra Gupta <102523463+Kushagra-Gupta-0103@users.noreply.github.com>
2022-04-07 18:37:50 +0530

    Update assg1-10-c.py

--- assignment-1/code/assg1-10-c.py
+++ assignment-1/code/assg1-10-c.py
@@ -5,47 +5,46 @@ Given: Angles of elevations QPR & PQT
 import matplotlib.pyplot as plt
 import numpy as np
 
-x='x'
-y='y'
 
 #--------------------------------
 #input parameters
 angle_QPR=np.pi/3
 angle_PQT=np.pi/6
 
-Q={x:40,y:0}
-R={x:40,y:50}
+h = 50 #length of QR
 #--------------------------------
+#setting up graph outline
 
-plt.xticks(list(range(-2,Q[x]+10,2)))
-plt.yticks(list(range(-2,R[y]+10,2)))
-
-plt.plot([Q[x],R[x]],[Q[y],R[y]])
-plt.annotate('Q',xy=(Q[x]+0.25,Q[y]))
-plt.annotate('R',xy=(R[x]+0.25,R[y]))
-
-P={x:0,y:0}
-P[x]=Q[x]-((R[y]-Q[y])/np.tan(angle_QPR))
-P[y]=Q[y]
-
-plt.plot([Q[x],P[x]],[Q[y],P[y]])
-plt.plot([R[x],P[x]],[R[y],P[y]])
-plt.annotate('P',xy=(P[x]-0.5,P[y]-1))
-
-T={x:0,y:0}
-T[x]=P[x]
-T[y]=P[y]+np.tan(angle_PQT)*(Q[x]-P[x])
-
-plt.plot([T[x],P[x]],[T[y],P[y]])
-plt.plot([T[x],Q[x]],[T[y],Q[y]])
-plt.annotate('T',xy=(T[x]-0.5,T[y]))
-
-PT=round(T[y]-P[y])
+plt.xticks(list(range(-2,44,2)))
+plt.yticks(list(range(-2,h+4,2)))
 plt.grid()
+#--------------------------------
+#plotting of lines and calculating lengths of other parameters
+plt.plot([40,40],[h,0])
 
+d = h/np.tan(angle_QPR)  # length of base PQ
+
+plt.plot([40,40-d],[0,0])
+plt.plot([40,40-d],[h,0])
+plt.annotate('P',xy=(40-d-0.5,-1))
+
+h2 = np.tan(angle_PQT)*(d)  # length of PT 
+
+plt.plot([40-d,40-d],[h2,0])
+plt.plot([40-d,40],[h2,0])
+
+#--------------------------------
+#labellings
+
+plt.annotate('Q',xy=(40+0.25,0))
+plt.annotate('R',xy=(40+0.25,h))
+plt.annotate('T',xy=(40-d-0.5,h2))
+plt.annotate('h',xy=(40.25,28))
+plt.annotate('d',xy=(25,-1.5))
+plt.annotate('h2',xy=(40-d-1,8))
+
+#---------------------------------
 #answer given in graph
-plt.title("PT = {}m after rounding off".format(PT))
+plt.title(f"PT = {round(h2)}m after rounding off")
 
 plt.show()
-
-
